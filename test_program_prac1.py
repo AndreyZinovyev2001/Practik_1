8f6c9926734e8fb46a5cd4bf259b75bedc12cf84
AndreyZinovyev2001 <82089177+AndreyZinovyev2001@users.noreply.github.com>
2024-02-25 18:28:40 +0300

    Create test_program_prac1.py

---
+++ test_program_prac1.py
@@ -0,0 +1,16 @@
+from program_prac1 import sqrNumber
+
+def test_sqr_5():
+    assert sqrNumber(5) == 25
+
+def test_sqr_8():
+    assert sqrNumber(5) == 64
+
+def test_sqr_0():
+    assert sqrNumber(0) == 0
+
+def test_sqr_10():
+    assert sqrNumber(5) == 100
+
+def test_sqr_1():
+    assert sqrNumber(5) == 1
