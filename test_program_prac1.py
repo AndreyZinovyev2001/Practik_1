eaec110d09fad2c2117bf42621c7aaef18a8722e
AndreyZinovyev2001 <82089177+AndreyZinovyev2001@users.noreply.github.com>
2024-02-25 19:20:14 +0300

    Update test_program_prac1.py

--- test_program_prac1.py
+++ test_program_prac1.py
@@ -4,13 +4,13 @@ def test_sqr_5():
     assert sqrNumber(5) == 25
 
 def test_sqr_8():
-    assert sqrNumber(5) == 64
+    assert sqrNumber(8) == 64
 
 def test_sqr_0():
     assert sqrNumber(0) == 0
 
 def test_sqr_10():
-    assert sqrNumber(5) == 100
+    assert sqrNumber(10) == 100
 
 def test_sqr_1():
-    assert sqrNumber(5) == 1
+    assert sqrNumber(1) == 1
