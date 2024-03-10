937f89a1ad9f5dd034f8c3c97cab13988a29f676
AndreyZinovyev2001 <82089177+AndreyZinovyev2001@users.noreply.github.com>
2024-03-10 20:22:43 +0300

    Update test_program_prac1.py

--- test_program_prac1.py
+++ test_program_prac1.py
@@ -1,16 +1,21 @@
+import unittest
 from program_prac1 import sqrNumber
 
-def test_sqr_5():
-    assert sqrNumber(5) == 25
+class TestSqrNumber(unittest.TestCase):
+    def test_sqr_5(self):
+        self.assertEqual(sqrNumber(5), 25)
 
-def test_sqr_8():
-    assert sqrNumber(8) == 64
+    def test_sqr_8(self):
+        self.assertEqual(sqrNumber(8), 64)
 
-def test_sqr_0():
-    assert sqrNumber(0) == 0
+    def test_sqr_0(self):
+        self.assertEqual(sqrNumber(0), 0)
 
-def test_sqr_10():
-    assert sqrNumber(10) == 100
+    def test_sqr_10(self):
+        self.assertEqual(sqrNumber(10), 100)
 
-def test_sqr_1():
-    assert sqrNumber(1) == 1
+    def test_sqr_1(self):
+        self.assertEqual(sqrNumber(1), 1)
+
+if __name__ == '__main__':
+    unittest.main()
